c4d82130cc549b0a5a57f2a6cc519eed7cb7c827
rohithdasariformal@gmail.com <rohithdasariformal@gmail.com>
2025-09-15 00:59:43 +0530

    clean up

--- app/app.go
+++ app/app.go
@@ -42,8 +42,13 @@ func SetupServer(db *sql.DB) *http.ServeMux {
 
 	// grades
 	mux.Handle("POST /api/v1/grades", middleware.JWTAuth(gradeHandler.AddGrade))
+
+
 	mux.Handle("GET /api/v1/grades", middleware.JWTAuth(gradeHandler.GetAverageOfClass))
+	
 	mux.Handle("GET /api/v1/grades/toppers", middleware.JWTAuth(gradeHandler.GetTopThree))
+
+
 	mux.Handle("PATCH /api/v1/grades", middleware.JWTAuth(gradeHandler.UpdateGrade))
 	return mux
 }
